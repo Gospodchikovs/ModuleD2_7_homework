486fa2ea2e450d325770886d01a796c808354e6c
Sergey Gospodchikov <uln@rambler.ru>
2023-02-15 07:41:16 +0400

    Переписал update_rating на aggregate(Sum())

--- news/models.py
+++ news/models.py
@@ -5,7 +5,7 @@ from django.db.models import Sum
 
 class Author(models.Model):
     rating = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     # обновление рейтинга автора
     def update_rating(self) -> int:
