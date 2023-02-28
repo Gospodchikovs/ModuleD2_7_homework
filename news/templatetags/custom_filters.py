17bc455df14a0c7097855d64b5f75042c27675b3
Sergey Gospodchikov <uln@rambler.ru>
2023-02-28 11:37:59 +0400

    Рефакторинг фильтра, страниц

--- news/templatetags/custom_filters.py
+++ news/templatetags/custom_filters.py
@@ -7,7 +7,7 @@ register = template.Library()
 @register.filter(name='censor')
 #
 # для фильтрации английских ругательств используем библиотеку profanity
-# для фильтрации русского мата можно использовать аналогичные библиотеки, например, https://github.com/PixxxeL/djantimat
+# для фильтрации русского мата можно использовать библиотеки, например, https://github.com/PixxxeL/djantimat
 #
 def censor(value):
     censored_text = profanity.censor(value)
