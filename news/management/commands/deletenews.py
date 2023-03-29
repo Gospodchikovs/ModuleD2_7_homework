585ade56974beb3204aec416df1dbc0cb256bf48
Sergey Gospodchikov <uln@rambler.ru>
2023-03-29 06:48:53 +0400

    Добавлены изменения по D10.4

--- news/management/commands/deletenews.py
+++ news/management/commands/deletenews.py
@@ -4,16 +4,16 @@ from news.models import Post, PostCategory
 
 class Command(BaseCommand):
     help = 'Удаление всех новстей'  # показывает подсказку при вводе "python manage.py <ваша команда> --help"
-    requires_migrations_checks = True  # напоминать ли о миграциях. Если true — то будет напоминание о том, что не сделаны все миграции (если такие есть)
+    requires_migrations_checks = True
+
     def handle(self, *args, **options):
         self.stdout.readable()
-        categories_list = PostCategory.objects.all().order_by('category')\
-            .values('category', 'category__topic').distinct()
+        categories_list = PostCategory.objects.all().order_by('category').distinct()
         self.stdout.write('Выберите код категории для удаления')
         cat_id = []
         for category in categories_list:
-            self.stdout.write(f"{category['category']} - {category['category__topic']}")
-            cat_id.append(category['category'])     # список категорий для выбора
+            self.stdout.write(f"{category.category_id} - {category.category.topic}")
+            cat_id.append(category.category)     # список категорий для выбора
         category_del = int(input())
         if category_del in cat_id:
             self.stdout.write('Вы действительно хотите удалить все новости выбранной категории {? yes/no')
