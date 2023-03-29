de1d5acec8b748a0b1d62f2ec18c87e16260436f
Sergey Gospodchikov <uln@rambler.ru>
2023-03-29 08:38:50 +0400

    Добавлены изменения по D10.4

--- news/management/commands/deletenews.py
+++ news/management/commands/deletenews.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from news.models import Post, PostCategory
+from news.models import Post, Category
 
 
 class Command(BaseCommand):
@@ -7,21 +7,21 @@ class Command(BaseCommand):
     requires_migrations_checks = True
 
     def handle(self, *args, **options):
-        categories_list = PostCategory.objects.all()
+        categories_list = Category.objects.all()
         self.stdout.readable()
         self.stdout.write('Выберите код категории для удаления')
-        for category in categories_list.values('category', 'category__topic').order_by('category').distinct():
-            self.stdout.write(f"{category['category']} - {category['category__topic']}")
+        for category in categories_list:
+            self.stdout.write(f"{category.id} - {category.topic}")
         try:
             category_for_del = int(input())
         except ValueError:
-            self.stdout.write(self.style.ERROR('Ошиька ввода категории! Удаление не произведено.'))
+            self.stdout.write(self.style.ERROR('Ошибка ввода категории! Удаление не произведено.'))
         else:
-            if categories_list.filter(category_id=category_for_del).exists():
+            if categories_list.filter(id=category_for_del).exists():
                 self.stdout.write('Вы действительно хотите удалить все новости выбранной категории? yes/No')
                 if input() == 'yes':
                     Post.objects.filter(category__id=category_for_del).delete()
-                    self.stdout.write(self.style.SUCCESS('Успешное удаление новстей!'))
+                    self.stdout.write(self.style.SUCCESS('Успешное удалено новостей'))
                     return
             self.stdout.write(self.style.ERROR('Удаление не произведено!'))
         return
